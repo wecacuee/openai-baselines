a077c9ebd7ce4b52013e902d591410cbc7a4af2a
Vikas Dhiman <dhiman@umich.edu>
2018-09-13 19:14:03 -0400

    FetchPush and slide do not have v0

--- baselines/her/experiment/train_many.py
+++ baselines/her/experiment/train_many.py
@@ -14,7 +14,8 @@ from ..results_plotter import plot_results
 
 def config_variations(
         keys = ["env", "addnl_loss_term"],
-        env = ["FetchReach-v1", "FetchPush-v0", "FetchSlide-v0"],
+        env = [#"FetchReach-v1",
+               "FetchPush-v1", "FetchSlide-v1"],
         addnl_loss_term = ["fwrl", "noop"]):
     kwargs = locals()
     return {k: kwargs[k] for k in keys}
